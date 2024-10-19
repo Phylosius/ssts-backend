231fceaf7383d73a805ddbc13470be5eb0934d0b
Phylosius <hei.phylosius@gmail.com>
2024-10-19 10:47:18 +0300

    feat: set used database into postgres

--- ssts_api/settings.py
+++ ssts_api/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-((imhjcn3$hpz#(tq3ycc-6z%_epf73l)1kv29$^k(r%&*^igg
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,17 +67,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ssts_api.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ssts_db',  # Le nom de ta base de données
+        'USER': 'ssts_db_user',  # Le nom de ton utilisateur PostgreSQL
+        'PASSWORD': 'ssts manager password',  # Le mot de passe que tu as défini
+        'HOST': 'localhost',  # Si PostgreSQL est en local, sinon l'adresse IP du serveur
+        'PORT': '5432',  # Port PostgreSQL par défaut
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -99,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -110,7 +109,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
