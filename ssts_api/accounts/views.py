9bb1eff4d1a767e50fec29b93a2d6e93a0134ff7
Phylosius <hei.phylosius@gmail.com>
2024-11-14 14:30:12 +0300

    refactor: (accounts) renamed views.AccountViewSet into UserViewSet

--- ssts_api/accounts/views.py
+++ ssts_api/accounts/views.py
@@ -6,7 +6,7 @@ from accounts.models import User
 from accounts.serializers import UserSerializer
 
 
-class AccountViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
