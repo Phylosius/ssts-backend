da6990f162b446dbe4bc7b14b74c32b648b30607
Phylosius <hei.phylosius@gmail.com>
2024-11-28 15:30:42 +0300

    feat: added id field to faces.Face

--- ssts_api/faces/models.py
+++ ssts_api/faces/models.py
@@ -1,8 +1,13 @@
+import uuid
+
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django.db.models import UUIDField
 
 
 class Face(models.Model):
+
+    id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     account = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
 
