cf26a6022c18a897a1c5750f241234c809043fe8
Phylosius <hei.phylosius@gmail.com>
2024-11-28 15:28:09 +0300

    feat: created faces.Face

--- ssts_api/faces/models.py
+++ ssts_api/faces/models.py
@@ -1,3 +1,14 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class Face(models.Model):
+
+    account = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+
+    # field for store face encoding given by face_recognition
+    encoding = ArrayField(  # only supported by a Postgresql database
+        models.FloatField(),
+        size=128,
+    )
+    recording_date = models.DateTimeField(auto_now_add=True)
