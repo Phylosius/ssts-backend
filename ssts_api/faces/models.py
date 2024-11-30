6ae7847e28c34b1481ebbde3af60e5cbb289014c
Phylosius <hei.phylosius@gmail.com>
2024-11-30 11:24:00 +0300

    feat: (faces.Face) added field reference_image

--- ssts_api/faces/models.py
+++ ssts_api/faces/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.db.models import UUIDField
@@ -9,7 +10,7 @@ class Face(models.Model):
 
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    account = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     # field for store face encoding given by face_recognition
     encoding = ArrayField(  # only supported by a Postgresql database
@@ -17,3 +18,7 @@ class Face(models.Model):
         size=128,
     )
     recording_date = models.DateTimeField(auto_now_add=True)
+    reference_image = models.ImageField(upload_to='faces/reference_images/', null=True, blank=True)
+
+    def __str__(self):
+        return f"Face of {self.account.username}"
