cef8f70d089bc62aaec237dddfe8301ded05e6a6
Daniel Langio <langio.tehiniavo@gmail.com>
2025-07-30 22:15:07 +0300

    feat(api): create account model

---
+++ api/model/account.py
@@ -0,0 +1,8 @@
+
+class Account:
+
+    def __init__(self, id: str, username: str, email: str, password: str):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password = password
