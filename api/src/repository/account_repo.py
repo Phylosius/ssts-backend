9f8c3542c4c8c5f807f5b8c9de1ac6a6dc66b048
Daniel Langio <langio.tehiniavo@gmail.com>
2025-07-31 04:27:17 +0300

    chore: create repository for account and face_id

--- api/src/repository/account_repo.py
+++ api/src/repository/account_repo.py
@@ -7,7 +7,6 @@ def save(cur, account: Account):
         "INSERT INTO account (id, username, email, password) VALUES (%s, %s, %s, %s);",
         (account.id, account.username, account.email, account.password),
     )
-
     return True
 
 @transactional
@@ -16,5 +15,28 @@ def delete(cur, account_id: str):
         "DELETE FROM account WHERE id = %s;",
         (account_id,),
     )
-
     return True
+
+@transactional
+def get_by_id(cur, account_id: str):
+    cur.execute(
+        "SELECT id, username, email, password FROM account WHERE id = %s;",
+        (account_id,),
+    )
+    data = cur.fetchone()
+    if data is None:
+        return None
+    data = list(data)
+    data.append(None)
+    return Account(*data)
+
+@transactional
+def update(cur, account_id: str, account: Account):
+    cur.execute(
+        "UPDATE account SET username = %s, email = %s, password = %s WHERE id = %s"
+        "RETURNING id, username, email, password;",
+        (account.username, account.email, account.password, account_id),
+    )
+    data = list(cur.fetchone())
+    data.append(None)
+    return Account(*data)
---
+++ api/src/repository/face_id_repo.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+import numpy as np
+
+from .pg import transactional
+from ..model.face_id import FaceId
+from ..mapper.array_mapper import ndarray_to_pgarray
+
+
+@transactional
+def save(cur, face_id: FaceId, account_id: str):
+    cur.execute(
+        "INSERT INTO face_id (id, account_id, added_at, updated_at, face_encodings) VALUES (%s, %s, %s, %s, %s);",
+        (face_id.id, account_id, face_id.created_at, face_id.updated_at, ndarray_to_pgarray(face_id.face_encodings)),
+    )
+    return True
+
+@transactional
+def delete(cur, face_id_id: str):
+    cur.execute(
+        "DELETE FROM face_id WHERE id = %s;",
+        (face_id_id,),
+    )
+    return True
+
+@transactional
+def get_by_id(cur, face_id_id: str):
+    cur.execute(
+        "SELECT id, added_at, updated_at, face_encodings FROM face_id WHERE id = %s;",
+        (face_id_id,),
+    )
+
+    data = cur.fetchone()
+    if data is None:
+        return None
+    return FaceId(*list(data))
+
+@transactional
+def update(cur, face_id_id: str, face_id: FaceId):
+    cur.execute(
+        "UPDATE face_id SET added_at = %s, updated_at = %s, face_encodings = %s WHERE id = %s"
+        "RETURNING id, added_at, updated_at, face_encodings;",
+        (face_id.created_at, face_id.updated_at, ndarray_to_pgarray(face_id.face_encodings), face_id_id),
+    )
+    data = list(cur.fetchone())
+    return FaceId(*data)
