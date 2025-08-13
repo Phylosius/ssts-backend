8dee72df96dfd955e4b5363403836938aa690d65
Daniel Langio <langio.tehiniavo@gmail.com>
2025-08-13 09:30:07 +0300

    chore: create Account.get_face_ids

--- api/src/model/account.py
+++ api/src/model/account.py
@@ -1,3 +1,4 @@
+from ..repository.face_id_repo import get_all_by_account_id as get_face_ids_by_account_id
 
 class Account:
 
@@ -7,5 +8,8 @@ class Account:
         self.email = email
         self.password = password
 
+    def get_face_ids(self):
+        return get_face_ids_by_account_id(self.id)
+
     def __str__(self):
-        return f'Account(id={self.id}, username={self.username}, email={self.email}, password={self.password}, face_id={self.face_id})'
+        return f'Account(id={self.id}, username={self.username}, email={self.email}, password={self.password})'
